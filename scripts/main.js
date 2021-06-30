c0dfe55747893ca15657dacf3d179acd37f6c2a7
Cody Zornes <cody@zornes.dev>
2021-06-30 11:46:02 -0700

    Add basic calculator functions per rq1.1

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+/**
+ * Calc me bby
+ * A calculator web app made for odin project fundamentals course
+ * Author: cody@zornes.dev
+ * Source: https://github.com/brrrdy/calc-me-bby
+ * Project reference/requirements: 
+ * https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/calculator
+ */
+
+/**
+ * Main entry point for app
+ */
+
+function main() {
+  console.log("Start!");
+}
+
+/**
+ * Returns the sum of two numbers
+ * 
+ * @param {number} n1 The first number to add.
+ * @param {number} n2 The second number to add.
+ * @returns {number} The sum of n1 and n2.
+ */
+function addition(n1, n2) {
+  return n1+n2;
+}
+console.log(`5 + 6 = ${addition(5,6)}`);
+
+/**
+ * Returns the result of subtraction between two numbers
+ * 
+ * @param {number} n1 The number to subtract from. 
+ * @param {number} n2 The amount to subtract from n1.
+ * @returns {number} The difference between n1 and n2.
+ */
+function subtraction(n1,n2) {
+  return n1 - n2;
+}
+
+console.log(`10 - 2 = ${subtraction(10,2)}`);
+
+/**
+ * Returns the result of multiplication between two numbers
+ * 
+ * @param {number} n1 The first number to multiply
+ * @param {number} n2 The second number to multiply
+ * @returns The multiplication result of n1 and n2
+ */
+function multiplication(n1,n2) {
+  return n1*n2;
+}
+console.log(`4 * 8 = ${multiplication(4,8)}`);
+
+/**
+ * Returns the result of division of n1 by n2
+ * 
+ * @param {number} n1 The dividend to divide
+ * @param {number} n2 The divisor to divide by
+ * @returns The quotient (result of division) of n1 by n2
+ */
+function division(n1,n2) {
+  return n1 / n2;
+}
+console.log(`64 / 16 = ${division(64,16)}`);
+
+main();
