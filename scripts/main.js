9938ef035d80e6ee5024e205222b0cb30f4ead9a
Cody Zornes <cody@zornes.dev>
2021-06-30 11:53:33 -0700

    add operation function per rq1.2

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,32 @@ function main() {
 }
 
 /**
+ * Perform an operation with the given operator and numbers
+ * 
+ * @param {string} op Operator to perform
+ * @param {number} n1 First operand
+ * @param {number} n2 Second operand
+ * @returns {number} The operation result
+ */
+
+function operate(op, n1, n2) {
+  switch(op) {
+    case "+":
+      return addition(n1,n2);
+      break;
+    case "-":
+      return subtraction(n1,n2);
+      break;
+    case "*":
+      return multiplication(n1,n2);
+      break;
+    case "/":
+      return division(n1,n2);
+      break;
+  }
+}
+
+/**
  * Returns the sum of two numbers
  * 
  * @param {number} n1 The first number to add.
@@ -23,9 +49,8 @@ function main() {
  * @returns {number} The sum of n1 and n2.
  */
 function addition(n1, n2) {
-  return n1+n2;
+  return n1 + n2;
 }
-console.log(`5 + 6 = ${addition(5,6)}`);
 
 /**
  * Returns the result of subtraction between two numbers
@@ -38,7 +63,6 @@ function subtraction(n1,n2) {
   return n1 - n2;
 }
 
-console.log(`10 - 2 = ${subtraction(10,2)}`);
 
 /**
  * Returns the result of multiplication between two numbers
@@ -48,9 +72,8 @@ console.log(`10 - 2 = ${subtraction(10,2)}`);
  * @returns The multiplication result of n1 and n2
  */
 function multiplication(n1,n2) {
-  return n1*n2;
+  return n1 * n2;
 }
-console.log(`4 * 8 = ${multiplication(4,8)}`);
 
 /**
  * Returns the result of division of n1 by n2
@@ -62,6 +85,14 @@ console.log(`4 * 8 = ${multiplication(4,8)}`);
 function division(n1,n2) {
   return n1 / n2;
 }
-console.log(`64 / 16 = ${division(64,16)}`);
+
+
+/**
+ *  Janky console testing zone
+ */
+console.log(`64 / 16 = ${operate("/",64,16)}`);
+console.log(`4 * 8 = ${operate("*",4,8)}`);
+console.log(`10 - 2 = ${operate("-",10,2)}`);
+console.log(`5 + 6 = ${operate("+",5,6)}`);
 
 main();
